bf6fca3113d67c982ada1b18899b00bc1c9b5abe
blaxtem <blaxtem@gmail.com>
2016-11-01 17:20:15 +0100

    no se donde lo he subido

--- app/src/main/java/com/example/janna/chat_2/MainActivity.java
+++ app/src/main/java/com/example/janna/chat_2/MainActivity.java
@@ -4,7 +4,6 @@ package com.example.janna.chat_2;
         import android.app.Activity;
         import android.database.DataSetObserver;
         import android.os.Bundle;
-        import android.renderscript.Element;
         import android.view.KeyEvent;
         import android.view.View;
         import android.widget.AbsListView;
@@ -12,74 +11,66 @@ package com.example.janna.chat_2;
         import android.widget.EditText;
         import android.widget.ImageButton;
         import android.widget.ListView;
-        import android.widget.TextView;
+        import android.widget.Toast;
 
-        import org.w3c.dom.Text;
-
-        import java.text.DateFormat;
-        import java.util.Date;
-        import java.util.Locale;
+        import butterknife.BindView;
+        import butterknife.ButterKnife;
+        import butterknife.OnClick;
 
 
 public class MainActivity extends Activity {
     private static final String TAG = "ChatActivity";
 
     private ChatArrayAdapter chatArrayAdapter;
-    private ListView listView;
-    private EditText chatText;
-    private ImageButton buttonSend;
-    private boolean side = false;
+
+    @BindView(R.id.msgview) ListView mListView;
+    @BindView(R.id.msg) EditText mChatText;
+    @BindView(R.id.send) ImageButton mButtonSend;
+    private boolean side;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-
-        buttonSend = (ImageButton) findViewById(R.id.send);
-        listView = (ListView) findViewById(R.id.msgview);
+        ButterKnife.bind(this);
 
         chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
-        listView.setAdapter(chatArrayAdapter);
+        mListView.setAdapter(chatArrayAdapter);
 
-        chatText = (EditText) findViewById(R.id.msg);
-        chatText.setOnKeyListener(new View.OnKeyListener() {
+        mChatText.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    return sendChatMessage(true);
+                    sendChatMessage(true);
+                    return true;
                 }
                 return false;
             }
         });
-        buttonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                if (chatText.getText().toString().trim().length() > 0){ /*miro si hi ha text per enviar*/
-                    sendChatMessage(true);
-                }
-            }
-        });
-
-
-        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
-        listView.setAdapter(chatArrayAdapter);
-
-        //to scroll the list view to bottom on data change
-        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
-            @Override
-            public void onChanged() {
-                super.onChanged();
-                listView.setSelection(chatArrayAdapter.getCount() - 1);
-            }
-        });
+        mListView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        mListView.setAdapter(chatArrayAdapter);
     }
 
-    private boolean sendChatMessage(Boolean esMio) {
+    @OnClick(R.id.send)
+    public void sendMessage(View view){
+        sendChatMessage(true);
+    }
 
-        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
-        side=!side;
-        chatText.setText("");
+
+    private boolean sendChatMessage(Boolean esMio) {
+        String mText = mChatText.getText().toString();
+        if(!mText.trim().matches("")){
+            chatArrayAdapter.add(new ChatMessage(esMio, mChatText.getText().toString()));
+            /*
+            INTERNET CODE
+
+
+             */
+        }else{
+            Toast.makeText(this,"Please enter something on the chat box", Toast.LENGTH_SHORT).show();
+        }
+
+        //side=!side;
+        mChatText.setText("");
         return true;
     }
 }
